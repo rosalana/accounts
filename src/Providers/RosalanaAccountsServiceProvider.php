9f6030947de13c4f8ca7ba1a1f149ba802c0fd29
Michael Bany <mobany526@gmail.com>
2025-02-01 17:04:04 +0100

    registrace facade

--- src/Providers/RosalanaAccountsServiceProvider.php
+++ src/Providers/RosalanaAccountsServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Rosalana\Core\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Rosalana\Accounts\Contracts\AuthContract;
+use Rosalana\Accounts\Services\AuthService;
 
 class RosalanaAccountsServiceProvider extends ServiceProvider
 {
@@ -11,6 +13,10 @@ class RosalanaAccountsServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->singleton(AuthContract::class, function($app) {
+            return new AuthService($app->make(\Rosalana\Accounts\Services\Client::class));
+        });
+
         // Co když chci dopsat do rosalana.php něco navíc? a nevytvářet nový?
         $this->mergeConfigFrom(__DIR__ . '/../../config/rosalana-accounts.php', 'rosalana');
     }
