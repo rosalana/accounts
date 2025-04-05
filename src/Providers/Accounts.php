e50bc371c53c0409d96301f0a5d7d49c7adf911f
Michael Bany <mobany526@gmail.com>
2025-04-05 17:00:45 +0200

    define publishing

--- src/Providers/Accounts.php
+++ src/Providers/Accounts.php
@@ -18,8 +18,14 @@ class Accounts implements Package
             'config' => [
                 'label' => 'Publish configuration settings to rosalana.php',
                 'run' => function () {
+
                     ConfigBuilder::new('accounts')
-                        ->add('test', 'Test value')
+                        ->add('model', 'App\\Models\\User::class')
+                        ->add('identifier', 'rosalana_account_id')
+                        ->comment(
+                            'Describe how Basecamp account is linked to your local Eloquent model. If you wish to change the identifier, make sure to update the provided database migration as well.',
+                            'Rosalana Basecamp Account Link'
+                        )
                         ->save();
                 }
             ]
