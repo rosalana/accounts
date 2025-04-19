34eef0b322552b548660204aab9043bb4370b788
Michael Bany <mobany526@gmail.com>
2025-04-19 13:17:20 +0200

    to post respo

--- stubs/routes/auth.php
+++ stubs/routes/auth.php
@@ -18,6 +18,6 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('logout', [AuthenticatedSessionController::class, 'destroy'])
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
